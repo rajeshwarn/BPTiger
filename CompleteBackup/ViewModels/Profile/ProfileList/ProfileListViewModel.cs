cc797a51a253e4ef65c0c2a49ca2747e3f3a06a5
guy <guy.keren@ncr.com>
2017-10-20 12:19:47 -0400

    Aded classes for profile list

---
+++ CompleteBackup/ViewModels/Profile/ProfileList/ProfileListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteBackup.ViewModels.Profile
+{
+    class ProfileListViewModel
+    {
+    }
+}
