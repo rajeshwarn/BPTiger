464a07f4c1105ee46622ed822fd419c56949d3db
guy <guy.keren@ncr.com>
2017-10-20 10:57:52 -0400

    changed name - profile to project

---
+++ CompleteBackup/Models/Backup/Project/BackupProjectData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteBackup.Models.Backup.Project
+{
+    public class BackupProjectData : ObservableObject
+    {
+        public string Name { get; set; } = "My Project";
+        public string Description { get { return Name; } set { } }
+        public ObservableCollection<BackupSetData> BackupSetList { get; set; } = new ObservableCollection<BackupSetData>();
+    }
+}
---
+++ CompleteBackup/Models/Backup/Project/BackupSetData.cs
@@ -0,0 +1,73 @@
+﻿using CompleteBackup.Models.Backup.Storage;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CompleteBackup.Models.Backup.Project
+{
+    public class BackupSetData : ObservableObject
+    {
+        public Guid GUID { get; set; } = Guid.Empty;
+        public string Name { get; set; } = "My Set";
+        public string Description { get { return Name; } set { } }
+
+        IStorageInterface m_IStorage = new FileSystemStorage();
+
+        public ObservableCollection<string> FolderList { get; set; } = new ObservableCollection<string>();
+
+        [XmlIgnore]
+        public bool IsValidSetData
+        {
+            get
+            {
+                bool bValidSet = false;
+                var subdirectoryList = GetDirectoriesNames(_TargetBackupFolder);
+
+                foreach (string subDirectory in subdirectoryList)
+                {
+                    string newPath = m_IStorage.Combine(_TargetBackupFolder, subDirectory);
+                    FileAttributes attr = m_IStorage.GetFileAttributes(newPath);
+
+                    if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                    {
+                        bValidSet |= subDirectory.StartsWith(GUID.ToString());
+                    }
+                }
+
+                return bValidSet;
+            }
+
+            private set { }
+        }
+
+        private string _TargetBackupFolder;
+        public string TargetBackupFolder { get { return _TargetBackupFolder; } set { _TargetBackupFolder = value; OnPropertyChanged(); } }
+
+        public List<string> GetDirectoriesNames(string path)
+        {
+
+            //Process directories
+            string[] sourceSubdirectoryEntries = m_IStorage.GetDirectories(path);
+            var sourceSubdirectoryEntriesList = new List<string>();
+            if (sourceSubdirectoryEntries != null)
+            {
+                if (sourceSubdirectoryEntriesList != null)
+                {
+                    foreach (var entry in sourceSubdirectoryEntries)
+                    {
+                        sourceSubdirectoryEntriesList.Add(m_IStorage.GetFileName(entry));
+                    }
+                }
+            }
+
+            return sourceSubdirectoryEntriesList;
+        }
+
+
+    }
+}
