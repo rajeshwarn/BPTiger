64bc1533b7253c1d104fecd218eda0918ef0a55f
guy <guy.keren@ncr.com>
2017-10-23 15:52:22 -0400

    added history

---
+++ CompleteBackup/Models/Backup/History/BackupSessionHistory.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteBackup.Models.Backup.History
+{
+    class BackupSessionHistory
+    {
+        List<string> AddedFiles { get; set; } = new List<string>();
+        List<string> ChangedFiles { get; set; } = new List<string>();
+        List<string> DeletedFiles { get; set; } = new List<string>();
+
+
+        public void 
+
+
+        public void SaveHistory(string path)
+        {
+            string json = json = JsonConvert.SerializeObject(this, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(path, json);
+            System.Diagnostics.Process.Start(path);
+        }
+    }
+}
