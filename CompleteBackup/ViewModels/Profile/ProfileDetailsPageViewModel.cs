2d40525244d02fec7c68359e43f88803f2f87bd3
guy <guy.keren@ncr.com>
2017-10-23 10:51:39 -0400

    Added gauge to details page

--- CompleteBackup/ViewModels/Profile/ProfileDetailsPageViewModel.cs
+++ CompleteBackup/ViewModels/Profile/ProfileDetailsPageViewModel.cs
@@ -1,15 +1,24 @@
 ﻿using CompleteBackup.DataRepository;
+using CompleteBackup.Views.ExtendedControls;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace CompleteBackup.ViewModels.Profile
 {
-    class ProfileDetailsPageViewModel
+    class ProfileDetailsPageViewModel : ObservableObject
     {
-        public BackupProjectRepository Repository { get; } = BackupProjectRepository.Instance;
+        public ProfileDetailsPageViewModel()
+        {
+//            ProfileGaugeList.Add(new ChartGaugeView(Brushes.Red, Brushes.Green, Brushes.Yellow) { PumpNumber = 0, GaugeValue = 66 });
+//            ProfileGaugeList.Add(new ChartGaugeView(Brushes.Red, Brushes.Green, Brushes.Yellow) { PumpNumber = 1, GaugeValue = 66 });
+        }
 
+        public BackupProjectRepository Repository { get; } = BackupProjectRepository.Instance;
+        public ObservableCollection<ChartGaugeView> ProfileGaugeList { get; set; } = new ObservableCollection<ChartGaugeView>();
     }
 }
