890fe28cbb8154414ba3a3955df9589542383cf0
Guy Keren <gkeren@gmail.com>
2017-10-29 16:36:06 -0400

    fixes

---
+++ CompleteBackup/Views/ActionsViews/Backup/BackupItemsSelectionView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using CompleteBackup.DataRepository;
+using CompleteBackup.Models.Backup.Storage;
+using CompleteBackup.Models.FolderSelection;
+using CompleteBackup.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompleteBackup.Views
+{
+    /// <summary>
+    /// Interaction logic for SourceDestinationItemsView.xaml
+    /// </summary>
+    public partial class SourceDestinationItemsView : UserControl
+    {
+        public SourceDestinationItemsView()
+        {
+            InitializeComponent();
+
+            //BindingExpression binding = txTaggetFolder.GetBindingExpression(TextBox.TextProperty);
+            //binding.UpdateSource();
+        }
+
+
+        //     private void hiddenNameTextBlock_TextChanged(object sender, TextChangedEventArgs e)
+        //     {
+        //         var profile = BackupProjectRepository.Instance.SelectedBackupProject?.CurrentBackupProfile;
+
+        //         var vm = this.DataContext as SourceDestinationItemsViewModel;
+        ////         vm.Init();
+        //     }
+
+    }
+}
