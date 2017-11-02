86e32092cc44c904bfb3d91928dd2ab6e65dd32c
Guy Keren <gkeren@gmail.com>
2017-11-02 00:42:27 -0400

    addded icon to list

--- CompleteBackup/Models/Profile/FolderData.cs
+++ CompleteBackup/Models/Profile/FolderData.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using CompleteBackup.Models.Backup.Storage;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Xml.Serialization;
 
 namespace CompleteBackup.Models.Backup.Profile
 {
@@ -19,5 +24,33 @@ namespace CompleteBackup.Models.Backup.Profile
 
         private long m_TotalSize { get; set; }
         public long TotalSize { get { return m_TotalSize; } set { m_TotalSize = value; OnPropertyChanged(); } }
+
+        [XmlIgnore]
+        public object Image
+        {
+            get
+            {
+                ImageSource imageSource = null;
+                try
+                {
+                    var m_IStorage = new FileSystemStorage();
+                    var icon = m_IStorage.ExtractIconFromPath(Path);
+                    imageSource = Imaging.CreateBitmapSourceFromHIcon(
+                        icon.Handle,
+                        System.Windows.Int32Rect.Empty,
+                        System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine($"Failed to get Icon {Path}\n{ex.Message}");
+                }
+
+                return imageSource;
+            }
+            private set
+            {
+                //m_Image = value;
+            }
+        }
     }
 }
