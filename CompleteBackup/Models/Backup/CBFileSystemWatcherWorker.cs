f3a7cdc25b41ff5bf7a7ac9cd8698697e25f1976
Guy Keren <gkeren@gmail.com>
2017-11-05 23:18:10 -0500

    addede renemaed file

---
+++ CompleteBackup/Models/Backup/CBFileSystemWatcherWorker.cs
@@ -0,0 +1,92 @@
+﻿using CompleteBackup.Models.Backup.Project;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteBackup.Models.Backup
+{
+    public class CBFileSystemWatcherWorker : BackgroundWorker
+    {
+        private CBFileSystemWatcherWorker() { }
+
+        public CBFileSystemWatcherWorker(BackupProjectData project)
+        {
+            WorkerReportsProgress = true;
+            WorkerSupportsCancellation = true;
+
+            DoWork += (sender, e) =>
+            {
+                try
+                {
+                    RunTask(@"E:\test2\source");
+                }
+                catch (TaskCanceledException ex)
+                {
+                    Trace.WriteLine($"Full Backup exception: {ex.Message}");
+                    e.Result = $"Full Backup exception: {ex.Message}";
+                    throw (ex);
+                }
+                finally
+                {
+                }
+            };
+        }
+    
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        protected void RunTask(string path)
+        {
+            //string[] args = System.Environment.GetCommandLineArgs();
+
+            //// If a directory is not specified, exit program.
+            //if (args.Length != 2)
+            //{
+            //    // Display the proper way to call the program.
+            //    Console.WriteLine("Usage: Watcher.exe (directory)");
+            //    return;
+            //}
+
+            // Create a new FileSystemWatcher and set its properties.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;// args[1];
+            /* Watch for changes in LastAccess and LastWrite times, and
+               the renaming of files or directories. */
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            // Only watch text files.
+            watcher.Filter = "*.*";
+
+            // Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+
+            // Wait for the user to quit the program.
+            Console.WriteLine("Press \'q\' to quit the sample.");
+          //  while (Console.Read() != 'q') ;
+        }
+
+        // Define the event handlers.
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+    }
+}
