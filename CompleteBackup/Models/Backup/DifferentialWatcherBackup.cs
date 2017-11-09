d7b57410e3d1fa706b06087f060d8f311423983f
Guy Keren <gkeren@gmail.com>
2017-11-09 08:17:08 -0500

    build gix

--- CompleteBackup/Models/Backup/DifferentialWatcherBackup.cs
+++ CompleteBackup/Models/Backup/DifferentialWatcherBackup.cs
@@ -20,141 +20,141 @@ namespace CompleteBackup.Models.backup
 
         protected override void ProcessDifferentialBackupRootFolders(string currentTargetPath, string lastTargetPath)
         {
-            foreach (var item in m_Profile.BackupWatcherItemList)
-            {
-                switch (item.ChangeType)
-                {
-                    case WatcherChangeTypes.Changed:
-                        {
-                            if (!m_IStorage.IsFolder(item.FullPath))
-                            {
-                                var newTargetPath = m_IStorage.Combine(m_IStorage.Combine(currentTargetPath, m_IStorage.GetFileName(item.WatchPath)), item.Name);
-                                if (!m_IStorage.FileExists(newTargetPath))
-                                {
-                                    m_Logger.Writeln($"**Update File, Warning - file does not exist, will copy the updated version {newTargetPath}");
+            //foreach (var item in m_Profile.BackupWatcherItemList)
+            //{
+            //    switch (item.ChangeType)
+            //    {
+            //        case WatcherChangeTypes.Changed:
+            //            {
+            //                if (!m_IStorage.IsFolder(item.FullPath))
+            //                {
+            //                    var newTargetPath = m_IStorage.Combine(m_IStorage.Combine(currentTargetPath, m_IStorage.GetFileName(item.WatchPath)), item.Name);
+            //                    if (!m_IStorage.FileExists(newTargetPath))
+            //                    {
+            //                        m_Logger.Writeln($"**Update File, Warning - file does not exist, will copy the updated version {newTargetPath}");
 
-                                    CopyFile(item.FullPath, newTargetPath);
-                                }
-                                else
-                                {
-                                    var newLastPath = m_IStorage.Combine(m_IStorage.Combine(lastTargetPath, m_IStorage.GetFileName(item.WatchPath)), item.Name);
+            //                        CopyFile(item.FullPath, newTargetPath);
+            //                    }
+            //                    else
+            //                    {
+            //                        var newLastPath = m_IStorage.Combine(m_IStorage.Combine(lastTargetPath, m_IStorage.GetFileName(item.WatchPath)), item.Name);
 
-                                    CopyFile(item.FullPath, newTargetPath, true);
-                                }
+            //                        CopyFile(item.FullPath, newTargetPath, true);
+            //                    }
 
-                            }
+            //                }
 
-                            break;
-                        }
+            //                break;
+            //            }
 
-                    case WatcherChangeTypes.Created:
-                        {
-                            var sourcePath = item.FullPath;
-                            var newTargetPath = m_IStorage.Combine(m_IStorage.Combine(targetPath, m_IStorage.GetFileName(item.WatchPath)), item.Name);
-                            if (m_IStorage.IsFolder(item.FullPath))
-                            {
-                                if (!m_IStorage.DirectoryExists(newTargetPath))
-                                {
-                                    CreateDirectory(newTargetPath);
-                                }
-                                else
-                                {
-                                    m_Logger.Writeln($"**Create Directory, Can't create - directory already exists {newTargetPath}");
-                                }
-                            }
-                            else
-                            {
-                                if (!m_IStorage.FileExists(newTargetPath))
-                                {
-                                    CopyFile(sourcePath, newTargetPath);
-                                }
-                                else
-                                {
-                                    m_Logger.Writeln($"**Create File, Can't create - file already exists {newTargetPath}");
-                                }
-                            }
+            //        case WatcherChangeTypes.Created:
+            //            {
+            //                var sourcePath = item.FullPath;
+            //                var newTargetPath = m_IStorage.Combine(m_IStorage.Combine(targetPath, m_IStorage.GetFileName(item.WatchPath)), item.Name);
+            //                if (m_IStorage.IsFolder(item.FullPath))
+            //                {
+            //                    if (!m_IStorage.DirectoryExists(newTargetPath))
+            //                    {
+            //                        CreateDirectory(newTargetPath);
+            //                    }
+            //                    else
+            //                    {
+            //                        m_Logger.Writeln($"**Create Directory, Can't create - directory already exists {newTargetPath}");
+            //                    }
+            //                }
+            //                else
+            //                {
+            //                    if (!m_IStorage.FileExists(newTargetPath))
+            //                    {
+            //                        CopyFile(sourcePath, newTargetPath);
+            //                    }
+            //                    else
+            //                    {
+            //                        m_Logger.Writeln($"**Create File, Can't create - file already exists {newTargetPath}");
+            //                    }
+            //                }
 
-                            break;
-                        }
+            //                break;
+            //            }
 
-                    case WatcherChangeTypes.Deleted:
-                        {
-                            var sourcePath = item.FullPath;
-                            var newTargetPath = m_IStorage.Combine(m_IStorage.Combine(targetPath, m_IStorage.GetFileName(item.WatchPath)), item.Name);
-                            if (m_IStorage.IsFolder(newTargetPath))
-                            {
-                                if (m_IStorage.DirectoryExists(newTargetPath))
-                                {
-                                    DeleteDirectory(newTargetPath);
-                                }
-                                else
-                                {
-                                    m_Logger.Writeln($"**Delete Directory, Can't delete - directory not found {newTargetPath}");
-                                }
-                            }
-                            else
-                            {
-                                if (m_IStorage.FileExists(newTargetPath))
-                                {
-                                    DeleteFile(newTargetPath);
-                                }
-                                else
-                                {
-                                    m_Logger.Writeln($"**Delete File, Can't delete - file not found {newTargetPath}");
-                                }
-                            }
-                            break;
-                        }
+            //        case WatcherChangeTypes.Deleted:
+            //            {
+            //                var sourcePath = item.FullPath;
+            //                var newTargetPath = m_IStorage.Combine(m_IStorage.Combine(targetPath, m_IStorage.GetFileName(item.WatchPath)), item.Name);
+            //                if (m_IStorage.IsFolder(newTargetPath))
+            //                {
+            //                    if (m_IStorage.DirectoryExists(newTargetPath))
+            //                    {
+            //                        DeleteDirectory(newTargetPath);
+            //                    }
+            //                    else
+            //                    {
+            //                        m_Logger.Writeln($"**Delete Directory, Can't delete - directory not found {newTargetPath}");
+            //                    }
+            //                }
+            //                else
+            //                {
+            //                    if (m_IStorage.FileExists(newTargetPath))
+            //                    {
+            //                        DeleteFile(newTargetPath);
+            //                    }
+            //                    else
+            //                    {
+            //                        m_Logger.Writeln($"**Delete File, Can't delete - file not found {newTargetPath}");
+            //                    }
+            //                }
+            //                break;
+            //            }
 
-                    case WatcherChangeTypes.Renamed:
-                        {
-                            if (m_IStorage.IsFolder(item.WatchPath))
-                            {
-                                var newTargetPath = m_IStorage.Combine(m_IStorage.Combine(targetPath, m_IStorage.GetFileName(item.WatchPath)), item.Name);
-                                var oldTargetPath = m_IStorage.Combine(m_IStorage.GetDirectoryName(newTargetPath), m_IStorage.GetFileName(item.OldPath));
+            //        case WatcherChangeTypes.Renamed:
+            //            {
+            //                if (m_IStorage.IsFolder(item.WatchPath))
+            //                {
+            //                    var newTargetPath = m_IStorage.Combine(m_IStorage.Combine(targetPath, m_IStorage.GetFileName(item.WatchPath)), item.Name);
+            //                    var oldTargetPath = m_IStorage.Combine(m_IStorage.GetDirectoryName(newTargetPath), m_IStorage.GetFileName(item.OldPath));
 
-                                if (m_IStorage.IsFolder(oldTargetPath))
-                                {
-                                    if (m_IStorage.DirectoryExists(oldTargetPath))
-                                    {
-                                        if (!m_IStorage.DirectoryExists(newTargetPath))
-                                        {
-                                            MoveDirectory(oldTargetPath, newTargetPath);
-                                        }
-                                        else
-                                        {
-                                            m_Logger.Writeln($"**Rename Directory, Can't rename - directory already exists {newTargetPath}");
-                                        }
-                                    }
-                                    else
-                                    {
-                                        m_Logger.Writeln($"**Rename Directory, Can't rename - Directory not found in backup folder {oldTargetPath}");
-                                    }
-                                }
-                                else if (m_IStorage.FileExists(oldTargetPath))
-                                {
-                                    if (!m_IStorage.FileExists(newTargetPath))
-                                    {
-                                        MoveFile(oldTargetPath, newTargetPath);
-                                    }
-                                    else
-                                    {
-                                        m_Logger.Writeln($"**Rename File, Can't rename - file already exists {newTargetPath}");
-                                    }
-                                }
-                                else
-                                {
-                                    m_Logger.Writeln($"**Rename file, Can't rename - file not found in backup folder {oldTargetPath}");
-                                }
-                            }
+            //                    if (m_IStorage.IsFolder(oldTargetPath))
+            //                    {
+            //                        if (m_IStorage.DirectoryExists(oldTargetPath))
+            //                        {
+            //                            if (!m_IStorage.DirectoryExists(newTargetPath))
+            //                            {
+            //                                MoveDirectory(oldTargetPath, newTargetPath);
+            //                            }
+            //                            else
+            //                            {
+            //                                m_Logger.Writeln($"**Rename Directory, Can't rename - directory already exists {newTargetPath}");
+            //                            }
+            //                        }
+            //                        else
+            //                        {
+            //                            m_Logger.Writeln($"**Rename Directory, Can't rename - Directory not found in backup folder {oldTargetPath}");
+            //                        }
+            //                    }
+            //                    else if (m_IStorage.FileExists(oldTargetPath))
+            //                    {
+            //                        if (!m_IStorage.FileExists(newTargetPath))
+            //                        {
+            //                            MoveFile(oldTargetPath, newTargetPath);
+            //                        }
+            //                        else
+            //                        {
+            //                            m_Logger.Writeln($"**Rename File, Can't rename - file already exists {newTargetPath}");
+            //                        }
+            //                    }
+            //                    else
+            //                    {
+            //                        m_Logger.Writeln($"**Rename file, Can't rename - file not found in backup folder {oldTargetPath}");
+            //                    }
+            //                }
 
-                            break;
-                        }
+            //                break;
+            //            }
 
-                    default:
-                        break;
-                }
-            }
+            //        default:
+            //            break;
+            //    }
+            //}
         }
     }
 }
