11079f8cad436addec19ae4a8dd18b6fcfc558ee
guy <guy.keren@ncr.com>
2017-11-10 10:40:49 -0500

    Refactoring

---
+++ CompleteBackup/Models/Backup/Managers/BackupWorkerTask.cs
@@ -0,0 +1,118 @@
+﻿using CompleteBackup.Models.backup;
+using CompleteBackup.Models.Backup.Profile;
+using CompleteBackup.Models.Backup.Storage;
+using CompleteBackup.Models.Utilities;
+using CompleteBackup.Views.MainWindow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CompleteBackup.Models.Profile
+{
+    public class BackupWorkerTask : BackgroundWorker
+    {
+        BackupBase m_BackupManager = null;
+
+        public bool? IsPaused { get {
+                bool? bpause = !m_BackupManager?.PauseWaitHandle.WaitOne(0);
+                return !m_BackupManager?.PauseWaitHandle.WaitOne(0); }
+            set {
+                if (value == true) {
+                    m_BackupManager?.PauseWaitHandle.Reset(); }
+                else {
+                    m_BackupManager?.PauseWaitHandle.Set(); }
+            } }
+
+
+        GenericStatusBarView m_ProgressBar;
+        BackupProfileData m_Profile;
+        protected BackupPerfectLogger m_Logger;
+
+        private BackupWorkerTask() { }
+
+        public BackupWorkerTask(BackupProfileData profile, bool bFullBackupScan)
+        {
+            m_Profile = profile;
+            m_ProgressBar = GenericStatusBarView.NewInstance;
+            m_Logger = profile.Logger;
+
+            WorkerReportsProgress = true;
+            WorkerSupportsCancellation = true;
+
+            DoWork += (sender, e) =>
+            {
+                profile.IsBackupWorkerBusy = true;
+                m_ProgressBar.UpdateProgressBar("Backup starting...", 0);
+                var startTime = DateTime.Now;
+                m_Logger.Writeln($"-------------------------------------------------");
+                try
+                {
+                    switch (profile.BackupType)
+                    {
+                        case BackupTypeEnum.Differential:
+                            if (bFullBackupScan)
+                            {
+                                m_Logger.Writeln($"Starting a Full Deep Differential backup");
+                                m_BackupManager = new DifferentialBackup(profile, m_ProgressBar);
+                            }
+                            else
+                            {
+                                m_Logger.Writeln($"Starting a Differential watcher backup");
+                                m_BackupManager = new DifferentialWatcherBackup(profile, m_ProgressBar);
+                            }
+                            break;
+
+                        case BackupTypeEnum.Incremental:
+                            if (bFullBackupScan)
+                            {
+                                m_Logger.Writeln($"Starting a Full Deep Incremental backup");
+                                m_BackupManager = new IncrementalFullBackup(profile, m_ProgressBar);
+                            }
+                            else
+                            {
+                                m_Logger.Writeln($"Starting Incremental watcher backup");
+                                m_BackupManager = new IncrementalFullWatcherBackup(profile, m_ProgressBar);
+                            }
+                            break;                            
+
+                        case BackupTypeEnum.Snapshot:
+                            m_Logger.Writeln($"Starting a Snapshot backup");
+                            m_BackupManager = new SnapshotBackup(profile, m_ProgressBar);
+                            break;
+
+                        default:
+                            throw new ArgumentNullException("Unknown backup type in BAckupWorker");
+                    }
+
+                    m_BackupManager.Init();
+                    m_BackupManager.ProcessBackup();
+                    m_BackupManager.Done();
+
+                    m_Profile.UpdateProfileProperties();
+
+                }
+                catch (TaskCanceledException ex)
+                {
+                    m_Logger.Writeln($"Backup exception: {ex.Message}");
+                    m_ProgressBar.UpdateProgressBar("Completed with Errors");
+                    m_ProgressBar.Release();
+                    Trace.WriteLine($"Full Backup exception: {ex.Message}");
+                    e.Result = $"Full Backup exception: {ex.Message}";
+                    throw (ex);
+                }
+                finally
+                {
+                    m_Logger.Writeln($"Backup completed, execution time: {DateTime.Now - startTime}");
+
+                    m_BackupManager = null;
+                    profile.IsBackupWorkerBusy = false;
+                }
+            };
+        }
+    }
+}
---
+++ CompleteBackup/Models/Backup/Managers/ProfileDataRefreshWorkerTask.cs
@@ -0,0 +1,213 @@
+﻿using CompleteBackup.Models.backup;
+using CompleteBackup.Models.Backup.History;
+using CompleteBackup.Models.Backup.Profile;
+using CompleteBackup.Models.Backup.Storage;
+using CompleteBackup.Utilities;
+using CompleteBackup.Views.MainWindow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CompleteBackup.Models.Profile
+{
+    public class ProfileDataRefreshWorkerTask : BackgroundWorker
+    {
+        //private BackupProfileData m_Profile;
+
+        private ProfileDataRefreshWorkerTask() { }
+
+        public ProfileDataRefreshWorkerTask(BackupProfileData profile)
+        {
+            //m_Profile = profile;
+
+            WorkerReportsProgress = true;
+            WorkerSupportsCancellation = true;
+
+            DoWork += (sender, e) =>
+            {
+                var storage = profile.GetStorageInterface();
+                try
+                {
+                    foreach (var item in profile.BackupFolderList)
+                    {
+                        if (item.IsFolder)
+                        {
+                            item.IsAvailable = storage.DirectoryExists(item.Path);
+                        }
+                        else
+                        {
+                            item.IsAvailable = storage.FileExists(item.Path);
+                        }
+                    }
+
+                    Application.Current.Dispatcher.Invoke(new Action(() =>
+                    {
+                        profile.BackupTargetDiskSize = "n/a";
+                        profile.BackupTargetUsedSize = "n/a";
+                        profile.BackupTargetFreeSize = "n/a";
+                    }));
+
+                    //Source folders sizes and number of files
+                    profile.BackupSourceFilesNumber = 0;
+                    profile.BackupSourceFoldersSize = 0;
+                    foreach (var item in profile.BackupFolderList.Where(i => i.IsAvailable))
+                    {
+                        if (item.IsFolder)
+                        {
+                            item.NumberOfFiles = storage.GetNumberOfFiles(item.Path);
+                            item.TotalSize = storage.GetSizeOfFiles(item.Path);
+                            Application.Current.Dispatcher.Invoke(new Action(() =>
+                            {
+                                profile.BackupSourceFilesNumber += item.NumberOfFiles;
+                                profile.BackupSourceFoldersSize += item.TotalSize;
+                            }));
+                        }
+                        else
+                        {
+                            Application.Current.Dispatcher.Invoke(new Action(() =>
+                            {
+                                profile.BackupSourceFilesNumber++;
+                            }));
+                        }
+                    }
+
+
+                    //Restore folders sizes and number of files
+                    profile.RestoreSourceFilesNumber = 0;
+                    profile.RestoreSourceFoldersSize = 0;
+                    foreach (var item in profile.RestoreFolderList)
+                    {
+                        if (item.IsFolder)
+                        {
+                            item.NumberOfFiles = storage.GetNumberOfFiles(item.Path);
+                            item.TotalSize = storage.GetSizeOfFiles(item.Path);
+                            Application.Current.Dispatcher.Invoke(new Action(() =>
+                            {
+                                profile.RestoreSourceFilesNumber += item.NumberOfFiles;
+                                profile.RestoreSourceFoldersSize += item.TotalSize;
+                            }));
+                        }
+                        else
+                        {
+                            Application.Current.Dispatcher.Invoke(new Action(() =>
+                            {
+                                profile.RestoreSourceFilesNumber++;
+                            }));
+                        }
+                    }
+
+
+                    if (profile.TargetBackupFolder != null)
+                    {
+                        //last backup time
+                        DateTime? lastTime = null;
+                        var lastSet = BackupBase.GetLastBackupSetName(profile);
+                        if (lastSet != null)
+                        {
+                            var sessionHistory = BackupSessionHistory.LoadHistory(profile.TargetBackupFolder, lastSet);
+                            lastTime = sessionHistory?.TimeStamp;
+                            Application.Current.Dispatcher.Invoke(new Action(() =>
+                            {
+                                profile.LastBackupDateTime = lastTime;
+                            }));
+                        }
+                    }
+
+                    if (profile.TargetBackupFolder != null)
+                    {
+                        profile.BackupTargetDiskSizeNumber = 0;
+                        //Target disk size
+                        string rootDrive = System.IO.Path.GetPathRoot(profile.TargetBackupFolder);
+                        foreach (System.IO.DriveInfo drive in System.IO.DriveInfo.GetDrives().Where(d => d.ToString().Contains(rootDrive)))
+                        {
+                            if (drive.IsReady)
+                            {
+                                profile.BackupTargetDiskSizeNumber = drive.TotalSize;
+                                Application.Current.Dispatcher.Invoke(new Action(() =>
+                                {
+                                    profile.BackupTargetDiskSize = FileFolderSizeHelper.GetNumberSizeString(profile.BackupTargetDiskSizeNumber);
+                                }));
+
+                                break;
+                            }
+                        }
+
+                        profile.BackupTargetUsedSizeNumber = 0;
+                        //Target used Space
+                        profile.BackupTargetUsedSizeNumber = storage.GetSizeOfFiles(profile.TargetBackupFolder);
+                        Application.Current.Dispatcher.Invoke(new Action(() =>
+                        {
+                            profile.BackupTargetUsedSize = FileFolderSizeHelper.GetNumberSizeString(profile.BackupTargetUsedSizeNumber);
+                        }));
+
+                        //Target free space
+                        profile.BackupTargetFreeSizeNumber = 0;
+                        //rootDrive = Path.GetPathRoot(profile.TargetBackupFolder);
+                        //foreach (DriveInfo drive in DriveInfo.GetDrives().Where(d => d.ToString().Contains(rootDrive)))
+                        //{
+                        //    if (drive.IsReady)
+                        //    {
+                        //        profile.BackupTargetFreeSizeNumber = drive.AvailableFreeSpace;
+                        //        //m_BackupTargetFreeSizeNumber = drive.TotalFreeSpace;
+                        //        Application.Current.Dispatcher.Invoke(new Action(() =>
+                        //        {
+                        //            profile.BackupTargetFreeSize = FileFolderSizeHelper.GetNumberSizeString(profile.BackupTargetFreeSizeNumber);
+                        //        }));
+
+                        //        break;
+                        //    }
+                        //}
+                    }
+
+                    ////Source Foldes Size
+                    //m_BackupSourceFoldersSizeNumber = 0;
+                    //foreach (var item in FolderList)
+                    //{
+                    //    m_BackupSourceFoldersSizeNumber += new DirectoryInfo(item.Path).GetFiles("*.*", SearchOption.AllDirectories).Sum(file => file.Length);
+                    //    Application.Current.Dispatcher.Invoke(new Action(() =>
+                    //    {
+                    //        BackupSourceFoldersSize = FileFolderSizeHelper.GetNumberSizeString(m_BackupSourceFoldersSizeNumber);
+                    //    }));
+                    //}
+
+                    if (m_ProfileDataUpdateEventCallback != null)
+                    {
+                        m_ProfileDataUpdateEventCallback(profile);
+                    }
+
+                    //update data to persistent storage
+                    DataRepository.BackupProjectRepository.Instance.SaveProject();
+                }
+                catch (TaskCanceledException ex)
+                {
+                    Trace.WriteLine($"Profile Data Update exception: {ex.Message}");
+                    e.Result = $"Profile Data Update exception: {ex.Message}";
+                    throw (ex);
+                }
+                finally
+                {
+                    profile.IsBackupWorkerBusy = false;
+                }
+            };
+        }
+
+        public delegate void ProfileDataUpdateEvent(BackupProfileData profile);
+        private event ProfileDataUpdateEvent m_ProfileDataUpdateEventCallback;
+
+        public void RegisterEvent(ProfileDataUpdateEvent callback)
+        {
+            m_ProfileDataUpdateEventCallback += callback;
+        }
+
+        public void UnRegisterEvent(ProfileDataUpdateEvent callback)
+        {
+            m_ProfileDataUpdateEventCallback -= callback;
+        }
+    }
+}
