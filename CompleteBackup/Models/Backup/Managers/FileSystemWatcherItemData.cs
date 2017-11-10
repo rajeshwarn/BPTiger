2941f0c55438e8e10c4c9c71359ffb05d23a3e1d
guy <guy.keren@ncr.com>
2017-11-10 17:06:12 -0500

    added missing file

---
+++ CompleteBackup/Models/Backup/Managers/FileSystemWatcherItemData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteBackup.Models.Backup
+{
+    public class FileSystemWatcherItemData
+    {
+        public string WatchPath { get; set; }
+
+        public DateTime Time { get; set; }
+        public WatcherChangeTypes ChangeType { get; set; }
+        public string OldPath { get; set; }
+        public string FullPath { get; set; }
+        public string Name { get; set; }
+
+    }
+}
