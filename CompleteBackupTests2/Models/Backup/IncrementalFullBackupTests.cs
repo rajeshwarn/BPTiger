bbc6f3c8f8163eabb7d839f09c72cd76666d50b5
guy <guy.keren@ncr.com>
2017-11-10 09:31:53 -0500

    Added test file

---
+++ CompleteBackupTests2/Models/Backup/IncrementalFullBackupTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompleteBackup.Models.backup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteBackup.Models.backup.Tests
+{
+    [TestClass()]
+    public class IncrementalFullBackupTests
+    {
+        [TestMethod()]
+        public void ProcessBackupTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IncrementalFullBackupTest()
+        {
+           // Assert.Fail();
+        }
+    }
+}
