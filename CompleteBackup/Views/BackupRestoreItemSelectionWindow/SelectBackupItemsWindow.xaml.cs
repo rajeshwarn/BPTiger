91173aef854b5bac3c73bb5ba049493bc867ad72
Guy Keren <gkeren@gmail.com>
2017-11-15 15:30:37 -0500

    cleanup

---
+++ CompleteBackup/Views/BackupRestoreItemSelectionWindow/SelectBackupItemsWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using CompleteBackup.Models.FolderSelection;
+using CompleteBackup.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CompleteBackup.Views
+{
+    /// <summary>
+    /// Interaction logic for SelectBackupItemsWindow.xaml
+    /// </summary>
+    public partial class SelectBackupItemsWindow : Window
+    {
+        public SelectBackupItemsWindow()
+        {
+            InitializeComponent();
+
+            var vm = DataContext as BackupItemsTreeBase;
+            vm?.InitItems();
+        }
+        private void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem tvi = e.OriginalSource as TreeViewItem;
+            var itemList = tvi.Items;
+
+            var vm = DataContext as SelectBackupItemsWindowModel;
+            vm.ExpandFolder(itemList);
+        }
+
+        private void FolderCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var checkBox = e.OriginalSource as CheckBox;
+
+            if (checkBox.IsChecked == null)
+            {
+                checkBox.IsChecked = false;
+            }
+
+            var dc = checkBox.DataContext as BackupFolderMenuItem;
+            var viewModel = DataContext as SelectBackupItemsWindowModel;
+            viewModel.FolderTreeClick(dc, (bool)checkBox.IsChecked);
+        }
+    }
+}
---
+++ CompleteBackup/Views/BackupRestoreView/CommonItemsSelectionView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CompleteBackup.DataRepository;
+using CompleteBackup.Models.Backup.Storage;
+using CompleteBackup.Models.FolderSelection;
+using CompleteBackup.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompleteBackup.Views
+{
+    /// <summary>
+    /// Interaction logic for CommonItemsSelectionView.xaml
+    /// </summary>
+    public partial class CommonItemsSelectionView : UserControl
+    {
+        public CommonItemsSelectionView()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var vm = DataContext as GenericBackupItemsSelectionViewModel;
+            vm.UpdateCurrentProfileChange();
+        }
+    }
+}
