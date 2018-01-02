8205ced934f8ef8d272a813583e400bc3bb97aa0
guy <guy.keren@ncr.com>
2018-01-02 16:37:56 -0500

    UI fix

--- CompleteBackup/ViewModels/ActionRestore/SelectRestoreItemsWindowModel.cs
+++ CompleteBackup/ViewModels/ActionRestore/SelectRestoreItemsWindowModel.cs
@@ -255,7 +255,7 @@ namespace CompleteBackup.ViewModels
             }
             else
             {
-                build errro
+                guyxxx build errro
                 ////History Items
                 //if (history != null)
                 //{
