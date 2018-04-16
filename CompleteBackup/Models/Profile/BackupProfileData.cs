4c83e67f6a79938051c547b8bc883aa1468c3670
guy <guy.keren@ncr.com>
2018-04-16 10:52:16 -0400

    cleanup

--- CompleteBackup/Models/Profile/BackupProfileData.cs
+++ CompleteBackup/Models/Profile/BackupProfileData.cs
@@ -26,8 +26,7 @@ namespace CompleteBackup.Models.Backup.Profile
     {
         [XmlIgnore]
         public static string TargetBackupBaseDirectoryName { get; } = "Archive";
-
-
+        
         //General properties
         private BackupTypeEnum m_BackupType { get; set; } = BackupTypeEnum.Snapshot;
         public BackupTypeEnum BackupType { get { return m_BackupType; } set { m_BackupType = value; OnPropertyChanged(); OnPropertyChanged("BackupTypeName"); OnPropertyChanged("BackupTypeImage"); } }
